0770d2b0478e148d66514628bf20bccc00943251
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-04-10 13:35:12 +0200

    Update resultat.php

--- test.loc/resultat.php
+++ test.loc/resultat.php
@@ -1,4 +1,15 @@
 <?php
+
 $score = $_GET['score'];
-echo "Gratulacje, zdałeś test, Twój wynik : $score / 10";
+$firstname = $_GET['firstname'];
+$lastname = $_GET['lastname'];
+$klass = $_GET['klass'];
+
+
+
+echo "Score: $score / 10 <br>";
+echo "First Name: $firstname <br>";
+echo "Last Name: $lastname <br>";
+echo "Class: $klass <br>";
+
 ?>
