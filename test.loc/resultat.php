601dd50feedf282738eca101ba1ac6cf50ab23d5
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-04-11 11:41:56 +0200

    Update resultat.php

--- test.loc/resultat.php
+++ test.loc/resultat.php
@@ -1,15 +1,15 @@
 <?php
+if (isset($_GET['score']) && isset($_GET['firstname']) && isset($_GET['lastname']) && isset($_GET['klass'])) {
+    $score = $_GET['score'];
+    $firstname = $_GET['firstname'];
+    $lastname = $_GET['lastname'];
+    $klass = $_GET['klass'];
 
-$score = $_GET['score'];
-$firstname = $_GET['firstname'];
-$lastname = $_GET['lastname'];
-$klass = $_GET['klass'];
-
-
-
-echo "Score: $score / 10 <br>";
-echo "First Name: $firstname <br>";
-echo "Last Name: $lastname <br>";
-echo "Class: $klass <br>";
-
+    echo "Score: $score / 10 <br>";
+    echo "First Name: $firstname <br>";
+    echo "Last Name: $lastname <br>";
+    echo "Class: $klass <br>";
+} else {
+    echo "One or more parameters are missing.";
+}
 ?>
