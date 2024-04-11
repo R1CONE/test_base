0e1cd07b72d60393325047a9bec08e9748f96fb6
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-04-11 14:09:51 +0200

    Update resultat.php

--- test.loc/resultat.php
+++ test.loc/resultat.php
@@ -1,15 +1,36 @@
 <?php
-if (isset($_GET['score']) && isset($_GET['firstname']) && isset($_GET['lastname']) && isset($_GET['klass'])) {
+if (isset($_GET['score'])) {
+
+    $servername = "localhost";
+    $username = "root";
+    $dbPassword = "";
+    $database = "tests_prog";
+    
+    $conn = new mysqli($servername, $username, $dbPassword, $database);
+    
+    if ($conn->connect_error) {
+        die("Błąd połączenia: " . $conn->connect_error);
+    }
+
     $score = $_GET['score'];
     $firstname = $_GET['firstname'];
     $lastname = $_GET['lastname'];
     $klass = $_GET['klass'];
 
+
     echo "Score: $score / 10 <br>";
     echo "First Name: $firstname <br>";
     echo "Last Name: $lastname <br>";
     echo "Class: $klass <br>";
-} else {
-    echo "One or more parameters are missing.";
+
+    $sql = "INSERT INTO resultat (Name, Lastname, klass, resultat) VALUES ('$firstname', '$lastname', '$klass', '$score')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Данные успешно добавлены в базу данных.";
+    } else {
+        echo "Ошибка: " . $conn->error;
+    }
+
+    $conn->close();
 }
 ?>
