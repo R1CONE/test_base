4c4fa17952472a7ded9068c3ee4cf213ab066f42
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-04-24 09:42:35 +0200

    Update resultat.php

--- test.loc/resultat.php
+++ test.loc/resultat.php
@@ -1,5 +1,11 @@
 <?php
-
+function decrypt($data, $password) {
+    $data = base64_decode($data);
+    $iv_length = openssl_cipher_iv_length($cipher="AES-256-CBC");
+    $iv = substr($data, 0, $iv_length);
+    $encrypted = substr($data, $iv_length);
+    return openssl_decrypt($encrypted, $cipher, $password, $options=0, $iv);
+}
 
 if(isset($_GET['score']) && isset($_GET['firstname']) && isset($_GET['lastname']) && isset($_GET['klass'])) {
 
@@ -13,19 +19,25 @@ if(isset($_GET['score']) && isset($_GET['firstname']) && isset($_GET['lastname']
     if ($conn->connect_error) {
         die("Błąd połączenia: " . $conn->connect_error);
     }
+
+    $password = "R1CONE";
     
     $score = $_GET['score'];
-    $firstname = $_GET['firstname'];
-    $lastname = $_GET['lastname'];
-    $klass = $_GET['klass'];
+    $encrypted_firstname = $_GET['firstname'];
+    $encrypted_lastname = $_GET['lastname'];
+    $encrypted_klass = $_GET['klass'];
 
+    $decrypted_firstname =  decrypt($encrypted_firstname, $password);
+    $decrypted_lastname = decrypt($encrypted_lastname, $password);
+    $decrypted_klass = decrypt($encrypted_klass, $password);
+    $decrypted_score = decrypt($score, $password); // Исправлено
 
-    echo "Score: $score / 10 <br>";
-    echo "First Name: $firstname <br>";
-    echo "Last Name: $lastname <br>";
-    echo "Class: $klass <br>";
+    echo "Score: $decrypted_score / 10 <br>";
+    echo "First Name: $decrypted_firstname <br>";
+    echo "Last Name: $decrypted_lastname <br>";
+    echo "Class: $decrypted_klass <br>";
 
-    $sql = "INSERT INTO resultat (Name, Lastname, klass, resultat) VALUES ('$firstname', '$lastname', '$klass', '$score')";
+    $sql = "INSERT INTO resultat (Name, Lastname, klass, resultat) VALUES ('$decrypted_firstname', '$decrypted_lastname', '$decrypted_klass', '$decrypted_score')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Данные успешно добавлены в базу данных.";
@@ -36,4 +48,3 @@ if(isset($_GET['score']) && isset($_GET['firstname']) && isset($_GET['lastname']
     $conn->close();
 }
 ?>
-
